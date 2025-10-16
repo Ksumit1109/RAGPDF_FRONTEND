54d155e1da759278a31a832ad104d3691ae36349
Sumit <ksumit1109@gmail.com>
2025-10-16 11:27:45 +0530

    Fix backend URL by removing trailing slash

--- constants/app.constant.ts
+++ constants/app.constant.ts
@@ -1,2 +1,2 @@
 // export const NEXT_PUBLIC_BACKEND_URL= "http://localhost:5000"
-export const NEXT_PUBLIC_BACKEND_URL= "https://ragpdf.onrender.com/"
+export const NEXT_PUBLIC_BACKEND_URL= "https://ragpdf.onrender.com"
