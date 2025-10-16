644642c9eb756d872e22dcf2370c38bad6269fcc
Sumit <ksumit1109@gmail.com>
2025-10-16 11:26:12 +0530

    Update backend URL to production endpoint

--- constants/app.constant.ts
+++ constants/app.constant.ts
@@ -1,1 +1,2 @@
-export const NEXT_PUBLIC_BACKEND_URL= "http://localhost:5000"
+// export const NEXT_PUBLIC_BACKEND_URL= "http://localhost:5000"
+export const NEXT_PUBLIC_BACKEND_URL= "https://ragpdf.onrender.com/"
